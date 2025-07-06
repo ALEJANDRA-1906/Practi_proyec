9db09dd6efe4a2122e0e88f1632ed965e794c012
vgarcia0906 <97006626+ALEJANDRA-1906@users.noreply.github.com>
2025-07-05 21:58:51 -0500

    creacion del modelo persona

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50) #Para campos ccn textos
+    apellido = models.CharField(max_length=10)
+    cedula = models.CharField(max_length=10, unique=True)
+    correo = models.EmailField(max_length=100)
+    edad = models.IntegerField(default=18)
